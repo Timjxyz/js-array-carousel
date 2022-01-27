e1608e05e4819f891400bac32da2e50dcae4b5fe
Timothy Gaither <timjgaither@gmail.com>
2022-01-27 15:07:53 +0100

    push esercizio

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const items = [
+const images = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg',
@@ -12,7 +12,7 @@ const title = [
     'Gran Bretagna',
     'Germania',
     'Paradise'
-]
+];
 
 const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
@@ -22,3 +22,77 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
+
+let slideScorrevoli='';
+let slides = '';
+
+for (let i = 0; i < images.length; i++) {
+
+    slides +=  `<div class="item">
+                    <img src="${images[i]}">
+                    <div class="text">
+                    <h3>${title[i]}</h3>
+                    <p>${text[i]}</p>
+                </div>
+                </div>`;
+
+     slideScorrevoli+= `<div class="item-scorrevoli">
+                            <img src="${images[i]}">
+                        </div>`
+                        
+
+                
+
+    
+
+    
+
+}
+
+//creo le slide
+let itemsContainer = document.querySelector('.slide-principale');
+itemsContainer.innerHTML=slides;
+console.log(itemsContainer)
+
+let slideContainer = document.querySelector('.slide-scorrevoli');
+slideContainer.innerHTML+=slideScorrevoli;
+console.log(itemsContainer)
+
+
+
+// inizializzo una variabile che mi tenga traccia della slide attiva
+let currentSlide = 0;
+
+let items = document.getElementsByClassName('item');
+items[currentSlide].classList.add('active');
+
+//Pulsante per scendere
+let next= document.getElementById("next");
+next.addEventListener('click',
+    function(){
+        if(currentSlide< items.length - 1){
+            items[currentSlide].classList.remove('active');
+            currentSlide++
+            items[currentSlide].classList.add('active');
+        }else{
+            items[currentSlide].classList.remove('active');
+            currentSlide=0;
+            items[currentSlide].classList.add('active');
+        }
+    }
+)
+//Pulsante per salire
+let prev= document.getElementById("prev");
+prev.addEventListener('click',
+    function(){
+        if(currentSlide> 0){
+            items[currentSlide].classList.remove('active');
+            currentSlide--
+            items[currentSlide].classList.add('active');
+        }else{
+            items[currentSlide].classList.remove('active');
+            currentSlide= items.length-1
+            items[currentSlide].classList.add('active');
+        }
+    }
+)
